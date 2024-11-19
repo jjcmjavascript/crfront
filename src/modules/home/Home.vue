f0e463757df55f3d8c8fd5ce33cfee8d4b77d1b6
Junior Corales <jjcmjavascript@gmail.com>
2024-11-19 18:32:59 -0300

    more cards

--- src/modules/home/Home.vue
+++ src/modules/home/Home.vue
@@ -5,64 +5,8 @@
 
   <section name="carousel section" alt="mas buscados">
     <h2 class="mas-buscado-text">Más buscados de la semana</h2>
-
-    <div class="carousel-section">
-      <div class="card-container">
-        <div>
-          <img
-            width="200rem"
-            height="200rem"
-            src="https://startupslatam.com/wp-content/uploads/2021/05/Lemontech.png"
-          />
-        </div>
-        <div class="companyCard-text-container">
-          <div class="companyCard-text">
-            <h5>Empresa:</h5>
-          </div>
-          <div class="companyCard-text">
-            <h5>Puntuacion:</h5>
-            <span>4.5 <i class="bi bi-star-fill"></i> </span>
-          </div>
-        </div>
-      </div>
-
-      <div class="card-container">
-        <div>
-          <img
-            width="200rem"
-            height="200rem"
-            src="https://startupslatam.com/wp-content/uploads/2021/05/Lemontech.png"
-          />
-        </div>
-        <div class="companyCard-text-container">
-          <div class="companyCard-text">
-            <h5>Empresa:</h5>
-          </div>
-          <div class="companyCard-text">
-            <h5>Puntuacion:</h5>
-            <span>4.5 <i class="bi bi-star-fill"></i> </span>
-          </div>
-        </div>
-      </div>
-
-      <div class="card-container">
-        <div>
-          <img
-            width="200rem"
-            height="200rem"
-            src="https://startupslatam.com/wp-content/uploads/2021/05/Lemontech.png"
-          />
-        </div>
-        <div class="companyCard-text-container">
-          <div class="companyCard-text">
-            <h5>Empresa:</h5>
-          </div>
-          <div class="companyCard-text">
-            <h5>Puntuacion:</h5>
-            <span>4.5 <i class="bi bi-star-fill"></i> </span>
-          </div>
-        </div>
-      </div>
+    <div class="cards-section-container">
+      <CompanyCard v-for="n in 5" />
     </div>
   </section>
 
@@ -75,18 +19,12 @@ import Carousel from '@/shared/components/carousel/Carousel.vue'
 import MyHeader from '@/shared/components/header/Header.vue'
 import SearchInput from './components/SearchSection.vue'
 import fakedata from '../../../fakedata'
+import CompanyCard from './components/CompanyCard.vue'
 
 const data = ref(fakedata)
 </script>
 
 <style>
-.carousel-section {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
 .mas-buscado-text {
   font-size: 3em;
   font-weight: bold;
@@ -105,7 +43,19 @@ const data = ref(fakedata)
   margin-top: 100px;
   height: 100px;
 }
+
+.cards-section-container {
+  margin-left: 10%;
+  width: 80%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  box-sizing: border-box;
+}
+
 @media (max-width: 768px) {
+  .mas-buscado-text {
+    font-size: 2em;
+  }
   .gradient-text {
     font-size: 1.5em;
   }
@@ -118,30 +68,9 @@ const data = ref(fakedata)
     width: 20%;
     font-size: 1rem;
   }
-}
 
-.card-container {
-  margin-left: 1rem;
-}
-
-.companyCard-text {
-  padding: 0px;
-  margin: 0px;
-  display: flex;
-  align-items: center;
-}
-.companyCard-text h5 {
-  margin-left: 1rem;
-  padding: 0;
-  margin: 0;
-  font-size: 1rem;
-}
-
-.companyCard-text span {
-  margin-left: 0.8rem;
-}
-
-.companyCard-text span i {
-  color: yellow;
+  .cards-section-container {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
---
+++ src/modules/home/components/CompanyCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card-container">
+    <div>
+      <img src="https://startupslatam.com/wp-content/uploads/2021/05/Lemontech.png" />
+    </div>
+    <div class="companyCard-text-container">
+      <div class="companyCard-text">
+        <h5>Empresa:</h5>
+      </div>
+      <div class="companyCard-text">
+        <h5>Puntuacion:</h5>
+        <span>4.5 <i class="bi bi-star-fill"></i> </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.companyCard-text {
+  padding: 0px;
+  margin: 0px;
+  display: flex;
+  align-items: center;
+}
+.companyCard-text h5 {
+  margin-left: 1rem;
+  padding: 0;
+  margin: 0;
+  font-size: 1rem;
+}
+
+.companyCard-text span {
+  margin-left: 0.8rem;
+}
+
+.companyCard-text span i {
+  color: yellow;
+}
+
+.card-container {
+  width: 100%;
+  margin-bottom: 1rem;
+  box-sizing: border-box;
+  padding: 5px;
+}
+
+img {
+  width: 100% !important;
+  margin-bottom: 1rem;
+}
+
+@media (max-width: 768px) {
+  .card-container {
+    width: 100%;
+    margin-bottom: 1rem;
+  }
+
+  img {
+    width: 100%;
+    height: 10rem;
+  }
+}
+</style>
