2a7bea02dcbebd1b76b185eb3ac05fe14a4c76e2
EduardoPerdomo <Dantelinksparda@gmail.com>
2024-12-03 10:34:38 -0400

    Finished

--- src/modules/company/Company.vue
+++ src/modules/company/Company.vue
@@ -1,15 +1,96 @@
 <template>
-<GridComponet/>
+  <div class="grid-container">
+    <div class="grid-item item-1">
+      <img
+        src="https://www.flaticon.com/media/dist/min/img/logos/flaticon-color-negative.svg"
+        alt="company"
+      />
+    </div>
+    <div class="grid-item item-2">
+      <ul>
+        <li><Ico />Salario<i class="bi bi-star-fill" v-for="i in 5" :key="i"></i></li>
+        <li>Tecnologías<i class="bi bi-star-fill" v-for="i in 5" :key="i"></i></li>
+        <li>Ambiente laboral<i class="bi bi-star-fill" v-for="i in 5" :key="i"></i></li>
+        <li>Posibilidad de crecimiento<i class="bi bi-star-fill" v-for="i in 5" :key="i"></i></li>
+      </ul>
+    </div>
+    <div class="grid-item item-3">
+      <div class="flex-container">
+        <Textbox />
+      </div>
+    </div>
+    <div class="grid-item item-4">
+      <div class="flex-container">
+        <form @submit.prevent="agregarPregunta">
+      <input
+        type="text"
+        v-model="nuevaPregunta"
+        placeholder="Inserte comentarios............"
+        required
+      />
 
+    </form>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup>
-import GridComponet from './GridComponent.vue'
+<script>
+import Textbox from './Texbox.vue'
 
+export default {
+  name: 'Company',
+  components: {
+    Textbox,
 
+  },
+}
 </script>
 
-<style>
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  gap: 10px; /* Puedes ajustar el valor del espacio entre celdas */
+}
 
+.grid-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #3153af5e; /* Color de fondo de los elementos */
+  padding: 5 px; /* Espaciado interno de los elementos */
+}
 
+.item-1 {
+  grid-column: span 3;
+  grid-row: span 3;
+}
+
+.item-2 {
+  grid-column: 4 / span 2;
+  grid-row: span 3;
+}
+
+.item-3 {
+  grid-column: span 5;
+  grid-row-start: 4;
+  align-items: center;
+}
+.item-4 {
+  grid-column: span 5;
+  grid-row-start: 5;
+
+}
+
+.flex-container {
+  display: flex;
+  gap: 4px; /* Ajusta el espacio entre el Textbox y el botón */
+  align-items: center;
+}
+
+.my-button:hover {
+  background-color: #45a049;
+}
 </style>
---
+++ src/modules/company/Texbox.vue
@@ -0,0 +1,74 @@
+
+<template>
+  <div class="preguntas-container">
+    <h2>Deja tu comentario</h2>
+    <form @submit.prevent="agregarPregunta">
+      <input
+        type="text"
+        v-model="nuevaPregunta"
+        placeholder="Escribe tu comentario aquí..."
+        required
+      />
+      <button type="submit">Comentar</button>
+    </form>
+    <ul>
+      <li v-for="pregunta in preguntas" :key="pregunta.id">
+        {{ pregunta.texto }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+
+form {
+
+  display: flex;
+  margin-bottom: 20px;
+}
+
+input[type="text"] {
+  flex-grow: 1;
+  width: 100%; /* Abarca todo el ancho del contenedor */
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px 0 0 5px;
+  font-size: 16px;
+
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+  font-size: 16px;
+
+}
+
+button:hover {
+  background-color: #45a049;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  background-color: #fff;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  border: 1px solid #eee;
+  font-size: 16px;
+}
+</style>
+
